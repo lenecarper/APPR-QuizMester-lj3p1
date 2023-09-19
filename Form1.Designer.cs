d03ba4f7ad565e9977aeee03b85c95dbf4a21538
Lene <lenecarpertunnel@gmail.com>
2023-09-19 10:19:45 +0200

    Add a login screen layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,213 @@ namespace APPR_QuizMester_lj3p1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.topFormPnl = new System.Windows.Forms.Panel();
+            this.closeFormBtn = new System.Windows.Forms.Button();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.loginFormPnl = new System.Windows.Forms.Panel();
+            this.txbUsernameLogin = new System.Windows.Forms.TextBox();
+            this.lblUsernameLogin = new System.Windows.Forms.Label();
+            this.lblPasswordLogin = new System.Windows.Forms.Label();
+            this.txbPasswordLogin = new System.Windows.Forms.TextBox();
+            this.linkForgotPassword = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkCreateAccount = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.topFormPnl.SuspendLayout();
+            this.loginFormPnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // topFormPnl
+            // 
+            this.topFormPnl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.topFormPnl.Controls.Add(this.lblLogin);
+            this.topFormPnl.Controls.Add(this.closeFormBtn);
+            this.topFormPnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topFormPnl.Location = new System.Drawing.Point(0, 0);
+            this.topFormPnl.Name = "topFormPnl";
+            this.topFormPnl.Size = new System.Drawing.Size(810, 37);
+            this.topFormPnl.TabIndex = 0;
+            // 
+            // closeFormBtn
+            // 
+            this.closeFormBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.closeFormBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("closeFormBtn.BackgroundImage")));
+            this.closeFormBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.closeFormBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeFormBtn.FlatAppearance.BorderSize = 0;
+            this.closeFormBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeFormBtn.Location = new System.Drawing.Point(747, 0);
+            this.closeFormBtn.Name = "closeFormBtn";
+            this.closeFormBtn.Size = new System.Drawing.Size(63, 37);
+            this.closeFormBtn.TabIndex = 0;
+            this.closeFormBtn.UseVisualStyleBackColor = false;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.Color.White;
+            this.lblLogin.Location = new System.Drawing.Point(3, 10);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(86, 20);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "QuizMester";
+            // 
+            // loginFormPnl
+            // 
+            this.loginFormPnl.BackColor = System.Drawing.Color.White;
+            this.loginFormPnl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.loginFormPnl.Controls.Add(this.pictureBox2);
+            this.loginFormPnl.Controls.Add(this.pictureBox1);
+            this.loginFormPnl.Controls.Add(this.linkCreateAccount);
+            this.loginFormPnl.Controls.Add(this.button1);
+            this.loginFormPnl.Controls.Add(this.linkForgotPassword);
+            this.loginFormPnl.Controls.Add(this.lblPasswordLogin);
+            this.loginFormPnl.Controls.Add(this.txbPasswordLogin);
+            this.loginFormPnl.Controls.Add(this.lblUsernameLogin);
+            this.loginFormPnl.Controls.Add(this.txbUsernameLogin);
+            this.loginFormPnl.Location = new System.Drawing.Point(49, 131);
+            this.loginFormPnl.Name = "loginFormPnl";
+            this.loginFormPnl.Size = new System.Drawing.Size(303, 304);
+            this.loginFormPnl.TabIndex = 1;
+            // 
+            // txbUsernameLogin
+            // 
+            this.txbUsernameLogin.Location = new System.Drawing.Point(61, 79);
+            this.txbUsernameLogin.Name = "txbUsernameLogin";
+            this.txbUsernameLogin.Size = new System.Drawing.Size(216, 22);
+            this.txbUsernameLogin.TabIndex = 2;
+            // 
+            // lblUsernameLogin
+            // 
+            this.lblUsernameLogin.AutoSize = true;
+            this.lblUsernameLogin.Location = new System.Drawing.Point(56, 59);
+            this.lblUsernameLogin.Name = "lblUsernameLogin";
+            this.lblUsernameLogin.Size = new System.Drawing.Size(73, 17);
+            this.lblUsernameLogin.TabIndex = 3;
+            this.lblUsernameLogin.Text = "Username";
+            // 
+            // lblPasswordLogin
+            // 
+            this.lblPasswordLogin.AutoSize = true;
+            this.lblPasswordLogin.Location = new System.Drawing.Point(58, 120);
+            this.lblPasswordLogin.Name = "lblPasswordLogin";
+            this.lblPasswordLogin.Size = new System.Drawing.Size(69, 17);
+            this.lblPasswordLogin.TabIndex = 5;
+            this.lblPasswordLogin.Text = "Password";
+            // 
+            // txbPasswordLogin
+            // 
+            this.txbPasswordLogin.Location = new System.Drawing.Point(61, 140);
+            this.txbPasswordLogin.Name = "txbPasswordLogin";
+            this.txbPasswordLogin.Size = new System.Drawing.Size(216, 22);
+            this.txbPasswordLogin.TabIndex = 4;
+            // 
+            // linkForgotPassword
+            // 
+            this.linkForgotPassword.AutoSize = true;
+            this.linkForgotPassword.Location = new System.Drawing.Point(77, 179);
+            this.linkForgotPassword.Name = "linkForgotPassword";
+            this.linkForgotPassword.Size = new System.Drawing.Size(153, 17);
+            this.linkForgotPassword.TabIndex = 6;
+            this.linkForgotPassword.TabStop = true;
+            this.linkForgotPassword.Text = "Forgot your password?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // linkCreateAccount
+            // 
+            this.linkCreateAccount.AutoSize = true;
+            this.linkCreateAccount.Location = new System.Drawing.Point(81, 256);
+            this.linkCreateAccount.Name = "linkCreateAccount";
+            this.linkCreateAccount.Size = new System.Drawing.Size(145, 17);
+            this.linkCreateAccount.TabIndex = 8;
+            this.linkCreateAccount.TabStop = true;
+            this.linkCreateAccount.Text = "Create a new account";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 51);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(4, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 51);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(810, 546);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(810, 546);
+            this.Controls.Add(this.loginFormPnl);
+            this.Controls.Add(this.topFormPnl);
+            this.Controls.Add(this.pictureBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.topFormPnl.ResumeLayout(false);
+            this.topFormPnl.PerformLayout();
+            this.loginFormPnl.ResumeLayout(false);
+            this.loginFormPnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel topFormPnl;
+        private System.Windows.Forms.Button closeFormBtn;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Panel loginFormPnl;
+        private System.Windows.Forms.Label lblUsernameLogin;
+        private System.Windows.Forms.TextBox txbUsernameLogin;
+        private System.Windows.Forms.Label lblPasswordLogin;
+        private System.Windows.Forms.TextBox txbPasswordLogin;
+        private System.Windows.Forms.LinkLabel linkForgotPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkCreateAccount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
