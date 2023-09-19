2bfcd59ba5eeb94e3a8d24a34af5a2b2b9ec2704
Lene <lenecarpertunnel@gmail.com>
2023-09-19 09:22:05 +0200

    Initial commit

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace APPR_QuizMester_lj3p1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
