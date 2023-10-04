d48243f6d3a2b9c07d679f155620c3df4a0bce18
Lene <lenecarpertunnel@gmail.com>
2023-10-04 09:47:32 +0200

    Add SQL client, quiz question class

--- Form2.cs
+++ Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,82 @@ namespace APPR_QuizMester_lj3p1
 {
     public partial class Form2 : Form
     {
+        private List<Question> questions = new List<Question>();
+        private int currentQuestionIndex = 0;
         public Form2()
         {
             InitializeComponent();
+            LoadQuestionsFromDatabase();
+            DisplayQuestion();
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             lblQuizQuestion.MaximumSize = new Size(250, 0);
             lblQuizQuestion.AutoSize = true;
+        }
+
+        private void LoadQuestionsFromDatabase()
+        {
+            string connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=QuizMesterDatabase;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT QuestionText, WrongAnswer1, WrongAnswer2, WrongAnswer3, CorrectAnswer FROM Questions";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string questionText = reader.GetString(0);
+                            string wrongAnswer1 = reader.GetString(1);
+                            string wrongAnswer2 = reader.GetString(2);
+                            string wrongAnswer3 = reader.GetString(3);
+                            string correctAnswer = reader.GetString(4);
+
+                            List<string> options = new List<string>
+                            {
+                                wrongAnswer1, wrongAnswer2, wrongAnswer3, correctAnswer
+                            };
+
+                            // Shuffle the options randomly
+                            Random rng = new Random();
+                            int n = options.Count;
+                            while (n > 1)
+                            {
+                                n--;
+                                int k = rng.Next(n + 1);
+                                string value = options[k];
+                                options[k] = options[n];
+                                options[n] = value;
+                            }
+
+                            Question question = new Question(questionText, options, correctAnswer);
+                            questions.Add(question);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DisplayQuestion()
+        {
+            if (currentQuestionIndex < questions.Count)
+            {
+                Question question = questions[currentQuestionIndex];
+                lblQuizQuestion.Text = question.Text;
+                btnAnswer1.Text = question.Options[0];
+                btnAnswer2.Text = question.Options[1];
+                btnAnswer3.Text = question.Options[2];
+                btnAnswer4.Text = question.Options[3];
+            }
+            else
+            {
+                MessageBox.Show("Quiz completed!");
+                // Handle quiz completion, e.g., show results or navigate to another form
+            }
         }
     }
 }
---
+++ Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPR_QuizMester_lj3p1
+{
+    public class Question
+    {
+        public string Text { get; }
+        public List<string> Options { get; }
+        public string CorrectAnswer { get; }
+
+        public Question(string text, List<string> options, string correctAnswer)
+        {
+            Text = text;
+            Options = options;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+}
